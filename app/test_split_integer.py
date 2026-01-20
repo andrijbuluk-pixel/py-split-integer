8514925084787534cf3d0eb53958e62a8c420bee
Andriy <andrijbuluk@gmail.com>
2026-01-20 20:25:14 +0100

    Split integer Fix Errors in assert

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,17 +6,16 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert split_integer(17, 4) == [4, 4, 4, 5]
+    assert split_integer(10, 2) == [5, 5]
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(17, 4) == [17]
+    assert split_integer(17, 1) == [17]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(2, 10)
-    assert sorted(result) == result
-
+    result = split_integer(32, 6)
+    assert result == sorted(result)
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result = split_integer(10, 2)
-    assert result[0] <= result[1]
+    result = split_integer(5, 10)
+    assert result.count(0) > 0
